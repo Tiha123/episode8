80e76190e6b41348a8829272102499f01e7b14dc
Tiha123 <waits6262@gmail.com>
2025-02-11 11:20:11 +0900

    v0.0.2] fix: smoothSpawn

--- Assets/_Scripts/Track.cs
+++ Assets/_Scripts/Track.cs
@@ -2,6 +2,25 @@ using UnityEngine;
 
 public class Track : MonoBehaviour
 {
-    public Transform EntryPoint;
-    public Transform ExitPoint;
+    [HideInInspector] public Transform EntryPoint;
+    [HideInInspector] public Transform ExitPoint;
+
+    [HideInInspector] public TrackManager trackmgr;
+
+    void LateUpdate()
+    {
+        scroll();
+    }
+
+    void scroll()
+    {
+        if (trackmgr != null)
+        {
+            transform.position += Vector3.back * Time.smoothDeltaTime * trackmgr.scrollspeed;
+        }
+        // Time.deltaTime 매 프레임당 1번 호출될때의 간격
+        // Time.fixedDeltaTime FixedUpdate(물리연산)기준, 0.02 간격(기본)
+        // Time.smoothDeltaTime deltaTime 평균, 값이 고르게 나옴
+        // fixed < delta < smooth
+    }
 }
--- Assets/_Scripts/TrackManager.cs
+++ Assets/_Scripts/TrackManager.cs
@@ -8,72 +8,74 @@ public class TrackManager : MonoBehaviour
 
     private List<Track> TrackList = new List<Track>();
 
-    [SerializeField, Range(0f, 50f)] float scrollspeed = 10f;
+    [Range(0f, 50f)] public float scrollspeed = 10f;
 
-    [SerializeField, Range(1, 10)] int TrackCount = 3;
+    [SerializeField, Range(1, 100)] int TrackCount = 3;
 
-    [SerializeField] float trackThreshold=0f;
+    [SerializeField] float trackThreshold = 0f;
 
-    int nameindex=0;
+    int nameindex = 0;
 
     Transform camTransform;
 
     void Start()
     {
-        camTransform=Camera.main.transform;
+        camTransform = Camera.main.transform;
         SpawnInitialTrack();
     }
 
     void Update()
     {
-        ScrollTrack();
+        // ScrollTrack();
         RepositionTrack();
     }
 
     void SpawnInitialTrack()
     {
+        Vector3 pos = Vector3.zero;
         for (int i = 0; i < TrackCount; ++i)
         {
-            Vector3 pos = i == 0 ? Vector3.zero : TrackList[i - 1].ExitPoint.position;
-            Track temp = Instantiate(TrackPrefab, pos, Quaternion.identity, transform);
-            TrackList.Add(temp);
-            TrackList[i].name = $"Track_{i}";
-            nameindex++;
+            Track temp = SpawnNextTrack(pos);
+            pos = temp.ExitPoint.position;
         }
     }
 
-    void ScrollTrack()
-    {
-        foreach (Track i in TrackList)
-        {
-            if (i != null)
-            {
-                i.transform.position += Vector3.back * Time.deltaTime * scrollspeed;
-            }
-        }
+    // void ScrollTrack()
+    // {
+    //     if (TrackList[0] != null)
+    //     {
+    //         foreach (Track i in TrackList)
+    //         {
 
-    }
+    //             i. transform.position += Vector3.back * Time.deltaTime * scrollspeed;
+
+    //         }
+    //     }
+
+    // }
 
     void RepositionTrack()
     {
-        if (TrackList.Count > 0)
+        if (TrackList.Count <= 0)
         {
-            if (TrackList[0].ExitPoint.position.z < camTransform.position.z-trackThreshold)
-            {
-
-                Destroy(TrackList[0].gameObject);
-                TrackList.RemoveAt(0);
-                SpawnNextTrack(TrackList[TrackList.Count-1]);
-            }
+            return;
+        }
+        if (TrackList[0].ExitPoint.position.z < camTransform.position.z - trackThreshold)
+        {
+            Destroy(TrackList[0].gameObject);
+            TrackList.RemoveAt(0);
+            Track lastTrack=TrackList[TrackList.Count - 1];
+            SpawnNextTrack(lastTrack.ExitPoint.position);
         }
     }
 
-    void SpawnNextTrack(Track current)
+    Track SpawnNextTrack(Vector3 pos)
     {
         Track temp;
-        temp = Instantiate(TrackPrefab, current.ExitPoint.position, Quaternion.identity, transform);
+        temp = Instantiate(TrackPrefab, pos, Quaternion.identity, transform);
+        temp.trackmgr = this;
         TrackList.Add(temp);
         temp.name = $"Track_{nameindex}";
-        nameindex++;
+        return temp;
     }
 }
