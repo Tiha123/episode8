c705b5f25bf0cefc634a9fb9d13bb3d2d3980946
Tiha123 <waits6262@gmail.com>
2025-03-07 10:08:13 +0900

    v0.1.35] feat: VersionCheck

--- Assets/_Scripts/Scene/SceneStart.cs
+++ Assets/_Scripts/Scene/SceneStart.cs
@@ -1,8 +1,19 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class SceneStart : MonoBehaviour
 {
+    [SerializeField] TextMeshProUGUI tmVersion;
+
+    void OnValidate()
+    {
+        if(tmVersion!=null)
+        {  
+            tmVersion.text=$"v{Application.version}";
+        }
+    }
+
     public void TapToStart()
     {
         // Debug.Log("TapToStart");
